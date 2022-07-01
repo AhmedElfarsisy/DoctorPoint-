f6495609b22d3a68a8964d3a701eb3edf271418a
ElFarsisyA <ahmed.elfarsisy@vodafone.com>
2022-07-01 21:35:17 +0200

    initial base view Model

---
+++ app/src/main/java/com/elfarsisy/doctorpoint/base/ui/viewmodel/DPBaseViewModel.kt
@@ -0,0 +1,8 @@
+package com.elfarsisy.doctorpoint.base.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@ViewModelScoped
+class DPBaseViewModel : ViewModel() {
+}

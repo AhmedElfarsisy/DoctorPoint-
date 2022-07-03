e02d191d898fd028184d1df5a28eb17931578443
ElFarsisyA <ahmed.elfarsisy@vodafone.com>
2022-07-03 19:45:45 +0200

    add icon helper

---
+++ app/src/main/java/com/elfarsisy/doctorpoint/helper/IconHelper.kt
@@ -0,0 +1,18 @@
+package com.elfarsisy.doctorpoint.helper
+
+import androidx.annotation.DrawableRes
+import com.elfarsisy.doctorpoint.R
+
+class IconHelper {
+    companion object {
+        @DrawableRes
+        fun getIconBy(iconId: Int) =
+            when (iconId) {
+                1 -> R.drawable.ic_logo
+                2 -> R.drawable.ic_onboarding1
+                3 -> R.drawable.ic_onboarding2
+                4 -> R.drawable.ic_onboarding3
+                else -> R.drawable.ic_logo_foreground
+            }
+    }
+}

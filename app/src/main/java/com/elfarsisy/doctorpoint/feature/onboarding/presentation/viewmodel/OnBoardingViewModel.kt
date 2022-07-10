ae5ba632f38abaf7f0842c338ba244cc0d488984
ElFarsisyA <ahmed.elfarsisy@vodafone.com>
2022-07-10 19:27:47 +0200

    remove unusable code

--- app/src/main/java/com/elfarsisy/doctorpoint/feature/onboarding/presentation/viewmodel/OnBoardingViewModel.kt
+++ app/src/main/java/com/elfarsisy/doctorpoint/feature/onboarding/presentation/viewmodel/OnBoardingViewModel.kt
@@ -9,21 +9,9 @@ import javax.inject.Inject
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor() : DPBaseViewModel() {
 
-    private var mPosition: Int = 0
-     val onBoardings = listOf<OnBoardingModel>(
+    val onBoardings = listOf<OnBoardingModel>(
         OnBoardingModel(2, R.string.onboarding_title1, R.string.onboarding_message1),
         OnBoardingModel(3, R.string.onboarding_title2, R.string.onboarding_message2),
         OnBoardingModel(4, R.string.onboarding_title3, R.string.onboarding_message3)
     )
-    var screenData: MutableLiveData<OnBoardingModel> = MutableLiveData()
-
-    fun updatePositionStatus(position: Int?) {
-        mPosition = position ?: 0
-        updateScreenData()
-    }
-
-    fun updateScreenData() {
-        screenData.value = onBoardings[mPosition]
-    }
-
 }

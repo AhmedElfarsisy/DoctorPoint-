df9ed9202e447116e4162c66afe42200b2b97218
ElFarsisyA <ahmed.elfarsisy@vodafone.com>
2022-08-27 20:50:46 +0200

    add sign in fragment # add fragment class # add fragment layout # add fragment UI match figma on sign in layout # add some assets icons and string attributes

--- app/src/main/java/com/elfarsisy/doctorpoint/feature/authentication/signin/view/SignInFragment.kt
+++ app/src/main/java/com/elfarsisy/doctorpoint/feature/authentication/signin/view/SignInFragment.kt
@@ -1,23 +1,23 @@
-package com.elfarsisy.doctorpoint.feature.authentication.startauth.view
+package com.elfarsisy.doctorpoint.feature.authentication.signin.view
 
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.elfarsisy.doctorpoint.R
 import com.elfarsisy.doctorpoint.base.ui.view.DPBaseFragment
-import com.elfarsisy.doctorpoint.databinding.FragmentStartAuthBinding
-import com.elfarsisy.doctorpoint.feature.authentication.startauth.viewmodel.StartAuthViewModel
+import com.elfarsisy.doctorpoint.databinding.FragmentSignInBinding
+import com.elfarsisy.doctorpoint.feature.authentication.signin.viewmodel.SignInViewModel
 
-class StartAuthFragment : DPBaseFragment<FragmentStartAuthBinding, StartAuthViewModel>() {
+class SignInFragment : DPBaseFragment<FragmentSignInBinding, SignInViewModel>() {
     override fun initViewModel() {
-        val mViewModel: StartAuthViewModel by activityViewModels()
+        val mViewModel: SignInViewModel by activityViewModels()
         viewModel = mViewModel
     }
 
-    override fun initBinding(container: ViewGroup?): FragmentStartAuthBinding {
+    override fun initBinding(container: ViewGroup?): FragmentSignInBinding {
         return DataBindingUtil.inflate(
             layoutInflater,
-            R.layout.fragment_start_auth,
+            R.layout.fragment_sign_in,
             container,
             false
         )
